ecfbdf29d7a27a371bfb33513114e1a31164105a
zhaoyanwen9 <1073159598@qq.com>
2021-01-11 22:20:27 +0800

    add task-ui

--- p-task/task-console/src/components/Login.vue
+++ p-task/task-console/src/components/Login.vue
@@ -15,98 +15,61 @@
 </template>
 
 <script>
-// Vuex 是一个专为 Vue.js应用程序开发的状态管理模式。它采用集中式存储管理应用的所有组件的状态，并以相应的规则保证状态以一种可预测的方式发生变化
-    import Vue from 'vue';
-    import Vuex from 'vuex';
-    Vue.use(Vuex);
-    export default new Vuex.Store({
-    state: {
-        isLogin: localStorage.getItem('token') ? true : false
-    },
-    mutations: {
-        setLoginState(state, bool) {
-            state.isLogin = bool;
-            // true 表示登录，false 表示注销
-        }
-    },
-    actions: {
-        login({commit}, user) {
-            // 发起登录请求，请求都拆分出去 service 文件夹中
-            // 使用 service 的请求方法
-            user.login(user).then(res => {
-                // 从 res.data 中解构数据，code和token
-                const { code, token } = res.data;
-                if (code) {
-                    // 登录成功，修改Vuex中间的登录状态
-                    commit('setLoginState', true);
-                    // 缓存令牌 token
-                    localStorage.setItem("token", token);
+    // Vuex 是一个专为 Vue.js应用程序开发的状态管理模式。它采用集中式存储管理应用的所有组件的状态，并以相应的规则保证状态以一种可预测的方式发生变化
+    export default {
+        name: "Login",
+        data() {
+            return {
+                ruleForm: {
+                    username: '',
+                    password: ''
+                },
+                rules: {
+                    password: [
+                        {required: true, trigger: 'blur'}
+                    ],
+                    username: [
+                        {required: true, trigger: 'blur'}
+                    ]
                 }
-                return code; // 返回code 给下一个 .then 使用
-            });
+            };
         },
-        logout({ commit }) {
-            // 清除缓存
-            localStorage.removeItem('token');
-            // 重置状态
-            commit('setLoginState', false);
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        const loading = this.$loading({
+                            lock: true,
+                            text: '登录中',
+                            spinner: 'el-icon-loading',
+                            background: 'rgba(0, 0, 0, 0.7)'
+                        });
+                        this.axios.post('/api/auth/login?username=admin1&password=admin1').then(response => {
+                            loading.close();
+                            if (response.status == 200) {
+                                // 登录成功后，重定向，如果没有则重定向到首页
+                                const path = this.$route.query.redirect || '/home';
+                                this.$router.push(path);
+                                this.$notify({
+                                    title: '成功',
+                                    message: '登录成功',
+                                    type: 'success'
+                                });
+                            }
+                        }).catch(function (error) {
+                            console.log(error);
+                        });
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
         }
     }
-});
-    //export default {
-    //    name: "Login",
-    //    data() {
-    //        return {
-    //            ruleForm: {
-    //                username: '',
-    //                password: ''
-    //            },
-    //            rules: {
-    //                password: [
-    //                    {required: true, trigger: 'blur'}
-    //                ],
-    //                username: [
-    //                    {required: true, trigger: 'blur'}
-    //                ]
-    //            }
-    //        };
-    //    },
-    //    methods: {
-    //        submitForm(formName) {
-    //            this.$refs[formName].validate((valid) => {
-    //                if (valid) {
-    //                    const loading = this.$loading({
-    //                        lock: true,
-    //                        text: '登录中',
-    //                        spinner: 'el-icon-loading',
-    //                        background: 'rgba(0, 0, 0, 0.7)'
-    //                    });
-    //                    this.axios.post('/api/auth/login?username=admin1&password=admin1').then(response => {
-    //                        loading.close();
-    //                        if (response.status == 200) {
-    //                            // 登录成功后，重定向，如果没有则重定向到首页
-    //                            const path = this.$route.query.redirect || '/home';
-    //                            this.$router.push(path);
-    //                            this.$notify({
-    //                                title: '成功',
-    //                                message: '登录成功',
-    //                                type: 'success'
-    //                            });
-    //                        }
-    //                    }).catch(function (error) {
-    //                        console.log(error);
-    //                    });
-    //                } else {
-    //                    console.log('error submit!!');
-    //                    return false;
-    //                }
-    //            });
-    //        },
-    //        resetForm(formName) {
-    //            this.$refs[formName].resetFields();
-    //        }
-    //    }
-    //}
 </script>
 <style scoped>
 
--- p-task/task-console/src/main.js
+++ p-task/task-console/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 import router from './router'
 import store from './store'
 
